4a740a5976e5d53216432d7ef26baf98d97da83a
ohsoyoung <soyoung466@naver.com>
2018-09-22 18:41:23 +0900

    continent trade

--- app/src/main/java/com/example/jeonghyeongkim/dong_geo/KakaoInputActivity.java
+++ app/src/main/java/com/example/jeonghyeongkim/dong_geo/KakaoInputActivity.java
@@ -90,6 +90,8 @@ public class KakaoInputActivity extends AppCompatActivity
                 String kakao = kakaoInput.getText().toString();
                 Toast.makeText(this, "카카오 아이디 : " + kakao, Toast.LENGTH_LONG).show();
 
+                Intent intent = new Intent(KakaoInputActivity.this, ContinentActivity.class);
+                startActivity(intent);
                 break;
         }
     }
