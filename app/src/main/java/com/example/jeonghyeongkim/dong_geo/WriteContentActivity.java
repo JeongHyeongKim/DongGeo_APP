5da83a7373b0ee26b5f4fd096bdba1f07a2640c2
고희경 <>
2018-09-29 22:09:20 +0900

    insert state

--- app/src/main/java/com/example/jeonghyeongkim/dong_geo/WriteContentActivity.java
+++ app/src/main/java/com/example/jeonghyeongkim/dong_geo/WriteContentActivity.java
@@ -40,7 +40,16 @@ import java.util.Date;
 public class WriteContentActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     private static Context context;
-    String[] exchangeRate = {"미국 USD", "일본 JPY", "중국 CNY", "유로 EUR", "영국 GBP", "캐나다 CAD", "홍콩 HKD", "스위스 CHF", "대만 TWD"};
+    String[] exchangeRate = {
+            "유럽연합 EUR",  "영국 GBP",  "스위스 CHF", "스웨덴 SEK", "체코 CZK", "덴마크 DKK", "노르웨이 NOK", "러시아 RUB", "폴란드 PLN", //유럽 코드 1
+            "일본 JPY", "중국 CNY", "홍콩 HKD", "대만 TWD",  "몽골 MNT", "카자흐스탄 KZT", "인도 INR","파키스탄 PKR", // 동아시아 코드 2
+            "태국 THB", "싱가포르 SGD", "말레이시아 MYR", "인도네시아 IDR",  "브루나이 BND", "베트남 VND",// 동남 아시아 코드 3
+            "오만 OMR",  "터키 TRY", "이스라엘 ILS", "사우디아라비아 SAR", "쿠웨이트 KWD", "바레 BHD",  "아랍에미리트 AED", "요르단 JOD",  "카타르 QAR", //중동 코드 4
+            "호주 AUD", "뉴질랜드 NZD", //오세아니아 코드 5
+            "캐나다 CAD", "미국 USD", //북아메리카 코드 6
+            "칠레 CLP", "브라질 BRL",  // 남아메리카 코드 7
+            "이집트 EGP", "남아공 ZAR", // 아프리카 코드 8
+            };
     String[] school_item = { "동덕여자대학교", "충북대학교", "서울대학교", "연세대학교", "고려대학교", "경희대학교", "한양대학교", "이화여자대학교" };
     EditText exchangeInput;
     EditText priceInput;
@@ -126,20 +135,46 @@ public class WriteContentActivity extends AppCompatActivity
         switch (v.getId()){
             case R.id.writeButton:
                 String exchange = exchangeInput.getText().toString();
+                //선택한 국가 대륙판단하기
+                int state = checkState(exchange);
+                Log.d("state", String.valueOf(state));
                 int amount = Integer.parseInt(priceInput.getText().toString());
                 String school =  schoolInput.getText().toString(); //지금은 한개지만, 향후 여러개로 수정해야할듯
 
 
                 long kakao_id=KakaoSignupActivity.get_kakao_id();
 
-                JSONObject jsonObject=MakeJson( exchange, amount, school, String.valueOf(kakao_id)); // 인증값은 0으로 테스트함
+                JSONObject jsonObject=MakeJson( exchange, state ,amount, school, String.valueOf(kakao_id)); // 인증값은 0으로 테스트함
                 PostData postData = new PostData(WriteContentActivity.this, jsonObject);
-                postData.execute("write_content.php");
+                postData.execute("http://write_content.php");
 
                 Toast.makeText(this, "통화 " + exchange + " 금액 " + amount + " 학교 " + school, Toast.LENGTH_LONG).show();
 //                Log.i("write", "price" + price + "exchange" + exchange);
                 break;
         }
+    }
+
+    private int checkState(String exchange) {
+
+       if( exchange.equals("유럽연합 EUR") ||  exchange.equals("영국 GBP") ||  exchange.equals("스위스 CHF") ||  exchange.equals("스웨덴 SEK") ||  exchange.equals("체코 CZK") ||  exchange.equals("덴마크 DKK") ||  exchange.equals("노르웨이 NOK") ||  exchange.equals("러시아 RUB") ||  exchange.equals("폴란드 PLN") )
+           return  1;//유럽 코드 1
+       else if( exchange.equals("일본 JPY") ||  exchange.equals("중국 CNY") ||  exchange.equals("홍콩 HKD") ||  exchange.equals("대만 TWD") ||   exchange.equals("몽골 MNT") ||  exchange.equals("카자흐스탄 KZT") ||  exchange.equals("인도 INR") ||  exchange.equals("파키스탄 PKR"))
+           return  2; // 북아시아 코드 2
+       else if( exchange.equals("태국 THB") ||  exchange.equals("싱가포르 SGD") ||  exchange.equals("말레이시아 MYR") ||  exchange.equals("인도네시아 IDR") ||   exchange.equals("브루나이 BND") ||  exchange.equals("베트남 VND"))
+           return  3;// 동남 아시아 코드 3
+       else if( exchange.equals("오만 OMR") ||  exchange.equals("터키 TRY") ||  exchange.equals("이스라엘 ILS") ||  exchange.equals("사우디아라비아 SAR") ||  exchange.equals("쿠웨이트 KWD") ||  exchange.equals("바레 BHD") ||   exchange.equals("아랍에미리트 AED") ||  exchange.equals("요르단 JOD") ||  exchange.equals( "카타르 QAR"))
+           return 4;//중동 코드 4
+       else if ( exchange.equals("호주 AUD") ||  exchange.equals("뉴질랜드 NZD"))
+           return 5;//오세아니아 코드 5
+       else if (  exchange.equals("캐나다 CAD") ||  exchange.equals("미국 USD"))
+           return 6;//북아메리카 코드 6
+       else if( exchange.equals("칠레 CLP") ||  exchange.equals("브라질 BRL"))
+           return 7;// 남아메리카 코드 7
+       else if(  exchange.equals("이집트 EGP") ||  exchange.equals("남아공 ZAR"))
+           return 8;// 아프리카 코드 8
+       else
+            return 0;
+
     }
 
     @Override
@@ -208,7 +243,7 @@ public class WriteContentActivity extends AppCompatActivity
         return true;
     } //
 
-    private JSONObject MakeJson(String exchange, int amount,  String school, String kakao_id){
+    private JSONObject MakeJson(String exchange, int state, int amount,  String school, String kakao_id){
         JSONObject jsonObject = new JSONObject(); //파라미터 데이터
 
         long now = System.currentTimeMillis();
@@ -218,6 +253,7 @@ public class WriteContentActivity extends AppCompatActivity
 
         try {
             jsonObject.put("currency", exchange);
+            jsonObject.put("state",state);
             jsonObject.put("amount", amount);
             jsonObject.put("university1", school);
             jsonObject.put("university2","NULL");
